e36e40c38f62365dad70df8368579e2aae34191d
Kenneth Ho <a0125364@u.nus.edu>
2016-08-29 03:05:17 +0800

    Updated to allow user to input words to ignore.

--- pipes-and-filters/src/KWIC.java
+++ pipes-and-filters/src/KWIC.java
@@ -1,12 +1,21 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import filters.*;
 import pipes.*;
 
 public class KWIC {
+	public static final int READER_PIPE_INDEX = 0;
+	public static final int REMOVER_PIPE_INDEX = 2;
+	public static final String IGNORE_WORD_MESSAGE = "Enter word(s) to ignore as keyword. Enter -1 to skip / end:";
+	
 	private ArrayList<Filter> filters;
+	private Scanner sc;
 	
 	public KWIC() {
+		sc = new Scanner(System.in);
 		filters = new ArrayList<Filter>();
+		
 		filters.add(new Reader());
 		filters.add(new CircularShifter());
 		filters.add(new Remover());
@@ -26,9 +35,33 @@ public class KWIC {
 	 * Runs the filters in kwic.
 	 */
 	public void execute() {
+		setup();
+		
 		for (Filter filter: filters) {
 			Thread thread = new Thread(filter);
 			thread.start();
+		}
+	}
+	
+	/**
+	 * To setup list of words to ignore.
+	 */
+	private void setup() {
+		Remover remover = (Remover) filters.get(REMOVER_PIPE_INDEX);
+		Reader reader = (Reader) filters.get(READER_PIPE_INDEX);
+		
+		reader.setScanner(sc);
+		
+		// Reads words to ignore.
+		System.out.println(IGNORE_WORD_MESSAGE);
+		while (sc.hasNext()) {
+			String input = sc.next();
+			
+			if (input.equalsIgnoreCase("-1")) {
+				break;
+			}
+			
+			remover.addIgnoredWord(input);
 		}
 	}
 	
